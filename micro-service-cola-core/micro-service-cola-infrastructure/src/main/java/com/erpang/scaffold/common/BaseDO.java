ea7f5dedabb2f90450460cf84c916f46888d8ea6
erpang <hnzhrh@gmail.com>
2024-10-29 19:27:27 +0800

    feat: Integrate mybatis plus.
    * Integrate the mybatis plus.
    * Profiles config.
    * The BaseDO to follow the Alibaba Java Development Guide.

---
+++ micro-service-cola-core/micro-service-cola-infrastructure/src/main/java/com/erpang/scaffold/common/BaseDO.java
@@ -0,0 +1,32 @@
+package com.erpang.scaffold.common;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * @author erpang
+ * @date 2024/10/29
+ */
+@Data
+public class BaseDO {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField(value = "gmt_create", fill = FieldFill.INSERT)
+    private LocalDate gmtCreate;
+
+    @TableField(value = "gmt_modified", fill = FieldFill.INSERT_UPDATE)
+    private LocalDate gmtModified;
+
+    @TableField("creator")
+    private String creator;
+
+    @TableField("modifier")
+    private String modifier;
+
+    @TableField("is_deleted")
+    @TableLogic
+    private Boolean isDeleted;
+}
