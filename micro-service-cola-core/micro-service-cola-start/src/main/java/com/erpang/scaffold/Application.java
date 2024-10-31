887bc59025d5b2a4bd82fe8a5e76f9d929469baa
erpang <hnzhrh@gmail.com>
2024-10-31 18:16:13 +0800

    feat: Integrate the nacos.
    1. Integrate the nacos.
    2. Update the application config.

--- micro-service-cola-core/micro-service-cola-start/src/main/java/com/erpang/scaffold/Application.java
+++ micro-service-cola-core/micro-service-cola-start/src/main/java/com/erpang/scaffold/Application.java
@@ -2,11 +2,18 @@ package com.erpang.scaffold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @author erpang
  * @date 2024/10/29
  */
+@EnableDiscoveryClient
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan(basePackages = {"com.erpang.scaffold.config"})
 @SpringBootApplication(scanBasePackages = {"com.erpang.scaffold", "com.alibaba.cola"})
 public class Application {
     public static void main(String[] args) {
